d0a5834f49235d1dba7c57bcb1bccd5ed7083f01
alichaudhary604 <achaudhary212009@gmail.com>
2026-05-02 22:43:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,7 @@ st.subheader("Available Tools")
 st.subheader("Ideas for future features should be told to me -Ali")
 st.markdown("""
 - **3D Visualizer:** View any molecule in 3D using SMILES.
-- Unit converters (coming soon)
-- molar mass calculator(just finished)
+- **Molar%ByMass* Calculator
+- **Concentration converter** mol/dm3 to g/dm3
+- **Interactive Periodic Table**
 """)
